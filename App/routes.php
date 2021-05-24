f4cb6e2a3b9a95d5b551d02bd2c238db29718559
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-05-24 12:03:26 +0800

    :fire: Test INSERT sql

--- App/routes.php
+++ App/routes.php
@@ -3,10 +3,15 @@
 use \Core\Http\Request;
 
 use \App\Models\User;
+
 $router->get('/', function () {
   $user = new User();
-  dd($user->insert([
+
+  // CREATE
+  $res = $user->insert([
     'email' => 'sample@mail.com',
     'password' => 'password'
-    ]));
+  ]);
+
+  dd($res);
 });
