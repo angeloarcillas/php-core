91f3dc108e7c89a3e01b55b1b4be62bb0cee0aea
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-06-25 09:09:10 +0800

    Add better format dd helper

--- src/helpers.php
+++ src/helpers.php
@@ -3,9 +3,22 @@
 /**
  *  Die and Dump
  */
-if (function_exists('dd')) {
+if (!function_exists('dd')) {
     function dd(...$data)
     {
         die(var_dump(...$data));
+    }
+}
+
+/**
+ *  Die, Dump and debug - better format
+ */
+if (!function_exists('ddd')) {
+    function ddd(...$data)
+    {
+        echo "<pre>";
+        var_dump(...$data);
+        echo "</pre>";
+        exit;
     }
 }
