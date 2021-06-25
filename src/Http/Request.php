ec30d067adb848b48f6b29e499c91a0c0b7a8370
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-06-25 15:22:41 +0800

    Add attributes to Request

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -5,6 +5,13 @@ namespace Zeretei\PHPCore\Http;
 class Request
 {
     /**
+     * attributes placeholder
+     * 
+     * @var array $attributes
+     */
+    protected $attributes = [];
+
+    /**
      * Request URI
      */
     public static function uri()
@@ -37,5 +44,19 @@ class Request
 
         // return specific get request
         return $_GET[$key] ?? null;
+    }
+
+    public function __get($key)
+    {
+        if (!array_key_exists($key, $this->attributes)) {
+            throw new \Exception("Property {$key} doesn't exists on class " . __CLASS__);
+        }
+
+        return $this->attributes[$key];
+    }
+
+    public function __set($key, $value)
+    {
+        $this->attributes[$key] = $value;
     }
 }
