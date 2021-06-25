188bfc267025f617d72352001d21ac28dc16c03c
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-06-25 09:13:30 +0800

    Create request uri method

--- public/index.php
+++ public/index.php
@@ -6,5 +6,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 require_once __DIR__ . '/../src/helpers.php';
 
-$m = Request::method();
-dd($m);
+$method = Request::method();
+$uri = Request::uri();
+dd($uri);
--- src/Request.php
+++ src/Request.php
@@ -4,6 +4,11 @@ namespace Zeretei\PHPCore;
 
 class Request
 {
+    public static function uri()
+    {
+        return $_SERVER['REQUEST_URI'];
+    }
+
     public static function method()
     {
         return $_SERVER['REQUEST_METHOD'];
