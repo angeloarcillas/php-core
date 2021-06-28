218f567f2948fac70bcfd9b02d99f42e8df2fc79
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-06-28 12:53:33 +0800

    Type cast & improve comments

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -9,12 +9,12 @@ class Request
      * 
      * @var array $attributes
      */
-    protected $attributes = [];
+    protected array $attributes = [];
 
     /**
      * Request URI
      */
-    public static function uri()
+    public static function uri(): string
     {
         // trimmed url
         $url = trim($_SERVER['REQUEST_URI'], '/');
@@ -26,18 +26,19 @@ class Request
     /**
      * Request method
      */
-    public static function method()
+    public static function method(): string
     {
-        // forced uppercase
+        // forced uppercase HTTP method
         return strtoupper($_SERVER['REQUEST_METHOD']);
     }
 
     /**
      * Request query string
      * 
-     * @param null|string $key
+     * @param string $key
+     * @return mixed
      */
-    public static function query($key = null)
+    public static function query(string $key = null): mixed
     {
         // if key is null, return all get request
         if (is_null($key)) return $_GET;
@@ -46,17 +47,31 @@ class Request
         return $_GET[$key] ?? null;
     }
 
-    public function __get($key)
+    /**
+     * Set Request attributes
+     * 
+     * @param string $key
+     * @param mixed $value
+     * @return void
+     */
+    public function __set(string $key, mixed $value): void
     {
+        $this->attributes[$key] = $value;
+    }
+
+    /**
+     * Get Request attribute
+     * 
+     * @param string $key
+     * @return mixed
+     */
+    public function __get(string $key): mixed
+    {
+        // check if $key exists on Request attributes
         if (!array_key_exists($key, $this->attributes)) {
             throw new \Exception("Property {$key} doesn't exists on class " . __CLASS__);
         }
 
         return $this->attributes[$key];
-    }
-
-    public function __set($key, $value)
-    {
-        $this->attributes[$key] = $value;
     }
 }
