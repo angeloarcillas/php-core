7e19019ffb4d31ca22524e51078ed7438503390b
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-06-28 12:48:47 +0800

    Type cast & improve comments

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -4,6 +4,11 @@ namespace Zeretei\PHPCore\Http;
 
 class Response
 {
+    /**
+     * HTTP status codes
+     * 
+     * @var int
+     */
     public const HTTP_OK = 200;
     public const HTTP_CREATED = 201;
     public const HTTP_NO_CONTENT = 204;
@@ -15,27 +20,46 @@ class Response
     public const HTTP_FORBIDDEN = 403;
     public const HTTP_NOT_FOUND = 404;
     public const HTTP_METHOD_NOT_ALLOWED = 405;
-    public const HTTP_REQUEST_TIMEOUT = 408;                                         // RFC6585
+    public const HTTP_REQUEST_TIMEOUT = 408;
     public const HTTP_TOO_MANY_REQUESTS = 429;
     public const HTTP_BAD_GATEWAY = 502;
     public const HTTP_SERVICE_UNAVAILABLE = 503;
 
-
-    public static function isValidCode($code)
+    /**
+     * Check if valid HTTP status code
+     * 
+     * @param int $code
+     * @return bool
+     */
+    public static function isValidCode(int $code): bool
     {
         return $code > 100 || $code <= 600;
     }
 
-    public static function setStatusCode($code)
+    /**
+     * Set response HTTP status code
+     * 
+     * @param int $code
+     * @return void
+     */
+    public static function setStatusCode(int $code): void
     {
+        // check if $code is a valid http status code
         if (!static::isValidCode($code)) {
             throw new \Exception(sprintf('The HTTP status code "%s"', $code));
         }
 
+        // set response code
         http_response_code($code);
     }
 
-    public function redirect($path, $status = self::HTTP_FOUND)
+    /**
+     * Redirect
+     * 
+     * @param string $path
+     * @param int $status
+     */
+    public function redirect(string $path, int $status = self::HTTP_FOUND)
     {
         // check if any header already sent
         if (!headers_sent()) {
