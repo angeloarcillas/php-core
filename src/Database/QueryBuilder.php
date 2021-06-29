90f1225fedada2c423609f25ed748b6ca6ab3892
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-06-29 13:20:55 +0800

    Remove connection.php & add todo

--- src/Database/QueryBuilder.php
+++ src/Database/QueryBuilder.php
@@ -2,113 +2,38 @@
 
 namespace Zeretei\PHPCore\Database;
 
-use \Exception;
-use \Zeretei\PHPCore\Database\Connection;
-
-abstract class QueryBuilder extends Connection
+abstract class QueryBuilder
 {
-    /**
-     * Valid fetch type
-     */
-    protected array $validFetchType = ['fetch', 'fetchAll'];
 
-    /**
-     * Raw SQL query from database
-     *
-     * @param string $sql
-     * @param array $params
-     * @return bool
-     */
-    protected function rawQuery(string $sql, array $params = []): bool
+    public function query($sql, $params = [])
     {
-        return $this->query(sql: $sql, params: $params);
+        $stmt = $this->conn()->prepare($sql);
+        return $stmt->execute($params);
     }
 
-    /**
-     * Raw SQL select query from database
-     *
-     * @param string $sql
-     * @param array $params
-     * @return mixed
-     */
-    protected function rawSelect(string $sql, array $params = []): mixed
+    public function fetch($sql, $params = [])
     {
-        return $this->query(sql: $sql, params: $params, fetchType: 'fetch');
-    }
-
-    /**
-     * Raw SQL select all query from database
-     *
-     * @param string $sql
-     * @param array $params
-     * @return array
-     */
-    protected function rawSelectAll(string $sql, array $params = []): array
-    {
-        return $this->query(sql: $sql, params: $params, fetchType: 'fetchAll');
-    }
-
-    /**
-     * Raw SQL row count query from database
-     *
-     * @param string $sql
-     * @param array $params
-     * @return int
-     */
-    protected function rawCount(string $sql, array $params = []): int
-    {
-        return $this->query(sql: $sql, params: $params, count: true);
-    }
-
-    /**
-     * Execute query
-     * 
-     * @param string $sql
-     * @param array $params
-     * @param null|string $fetchType
-     * @param bool $count
-     * @return mixed
-     */
-    private function query(
-        string $sql,
-        array $params = [],
-        ?string $fetchType = null,
-        bool $count = false
-    ): mixed {
-        // prepare sql
-        $stmt = $this->connection()->prepare($sql);
-
-        // get parameters
-        $params = array_values($params);
-
-        // if count, return the number of row selected
-        if ($count) {
-            $stmt->execute($params);
-            return $stmt->rowCount();
-        }
-
-        // if no fetch type then execute
-        if (!$fetchType) return $stmt->execute($params);
-
-        // if invalid fetch type, throw error
-        if (!in_array($fetchType, $this->validFetchType)) {
-            throw new Exception("Type \"{$fetchType}\" is an invalid Fetch Type.");
-        }
-
-        // execute sql
+        $stmt = $this->conn()->prepare($sql);
         $stmt->execute($params);
+        return $stmt->fetch();
+    }
 
-        // fetch from database
-        $result = $stmt->{$fetchType}();
-
-
-        // return fetched result
-        return $result;
+    public function fetchAll($sql, $params = [])
+    {
+        $stmt = $this->conn()->prepare($sql);
+        $stmt->execute($params);
+        return $stmt->fetchAll();
     }
 
     // Establish connection
-    private function connection()
+    private function conn()
     {
-        return parent::connect(app('config')['database']);
+        // TODO: revamp app to "Keep it simple approach"
+        return new \PDO(
+            $this->config['connection'] . ';dbname=' . $this->config['name'],
+            $this->config['username'],
+            $this->config['password'],
+            $this->config['options']
+        );
     }
 }
