4cf6632103f06a98553208b79465f6b2e30cd2f6
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-06-29 17:08:47 +0800

    Revamp model

--- src/blueprint/Model.php
+++ src/blueprint/Model.php
@@ -2,51 +2,84 @@
 
 namespace Zeretei\PHPCore\Blueprint;
 
-use \Zeretei\PHPCore\Database\QueryBuilder;
+use Zeretei\PHPCore\Application;
 
-abstract class Model extends QueryBuilder
+abstract class Model
 {
+    /**
+     * Database table name
+     */
     protected string $table;
+
+    /**
+     * Fillable inputs
+     */
     protected array $fillable = [];
+
+    /**
+     * SQL where selector key
+     */
     protected string $key = 'id';
 
     public function __construct()
-    {;
+    {
+        // set table
         if (!isset($this->table)) {
             $this->table = $this->getBaseClassname() . 's';
         }
     }
 
-        /**
-     * Select all rows from database | READ
-     *
-     * @param null|string $table
-     * @param array $columns
-     * @return array
+    /**
+     * Execute SQL Insert statement
      */
-    public function selectAll(array $columns = ['*'], string $table = null): array
+    public function insert(array $params): bool
     {
+        // filter request with $fillable
+        $params = $this->filter($params);
+
+        // set column names
+        $columns = implode(',', array_keys($params));
+
+        // set column values placeholder
+        $values = trim(str_repeat('?,', count($params)), ',');
+
+        // sql statement
         $sql = sprintf(
-            "SELECT %s FROM %s",
-            // set column names
-            implode(',', $columns),
-            // check if user defined a table
-            $table ?? $this->table
+            'INSERT INTO %s (%s) VALUES (%s)',
+            $this->table,
+            $columns,
+            $values
         );
 
-        return $this->rawSelectAll($sql);
+        return Application::get('database')->query($sql, array_values($params));
     }
 
+    // public function update($params): bool
+    // {
+    // }
+
+    // public function delete($params): bool
+    // {
+    // }
+
+    // public function select($params): array|false
+    // {
+    // }
+
+    // public function all(): array
+    // {
+    // }
 
     /**
      * Filter $request with $this->fillable
      * Returns all request that can be filled
-     * 
-     * @param array $params
-     * @return array
      */
     protected function filter(array $params): array
     {
+        if (empty($this->fillable)) {
+            throw new \Exception('$fillable must have a value.');
+        }
+
         return array_filter(
             // request
             $params,
@@ -62,7 +95,7 @@ abstract class Model extends QueryBuilder
     /**
      * Get base classname
      */
-    protected function getBaseClassname()
+    protected function getBaseClassname(): string
     {
         $class = get_called_class();
         $base = basename(str_replace('\\', DIRECTORY_SEPARATOR, $class));
