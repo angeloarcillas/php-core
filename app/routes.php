7928bc009fd76140f157e73d734e1c76dba214c6
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-06-30 08:16:20 +0800

    Create UserController resourceful routes

--- app/routes.php
+++ app/routes.php
@@ -9,12 +9,14 @@ return function (Router $router) {
         return view('welcome');
     });
 
+    // USERCONTROLLER
     $router->get('/users', [UserController::class, 'index']);
-    $router->get('/users/:int', [UserController::class, 'show']);
     $router->get('/users/create', [UserController::class, 'create']);
     $router->post('/users', [UserController::class, 'store']);
+    $router->get('/users/:int', [UserController::class, 'show']);
     $router->get('/users/:int/edit', [UserController::class, 'edit']);
-    $router->put('/users/:int/update', [UserController::class, 'update']);
+    $router->put('/users/:int', [UserController::class, 'update']);
+    $router->delete('/users/:int', [UserController::class, 'destroy']);
 
     $router->get('/migration', function () {
         $migration = new Migration();
