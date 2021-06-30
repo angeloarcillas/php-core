a224a7200cb86c4600e5908e0b5a92e9cdc2ec8f
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-06-30 11:00:19 +0800

    Add strict type and start session

--- public/index.php
+++ public/index.php
@@ -1,4 +1,12 @@
 <?php
+// no type coercion allowed
+declare(strict_types=1);
+
+// check if session started
+if (session_status() === PHP_SESSION_NONE) {
+    // start a session
+    session_start();
+}
 
 use Zeretei\PHPCore\Application;
 use Zeretei\PHPCore\Http\Request;
