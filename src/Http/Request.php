eddfe42be73e667c2081e573c74827bc38c913df
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-07-02 20:58:59 +0800

    Refactor

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -4,33 +4,22 @@ namespace Zeretei\PHPCore\Http;
 
 use \Zeretei\PHPCore\Http\Traits\Validator;
 
-/**
- * TODO:
- * 1. Improve security
- * 2. add request validation
- */
 class Request
 {
-
     use Validator;
+
     /**
      * attributes placeholder
-     * 
-     * @var array $attributes
      */
     protected array $attributes = [];
 
     public function __construct()
     {
-        // set attriutes and sanitize request
+        // sanitze request
         $this->attributes = array_map(function (string $request) {
-            // check if it's not a string
             if (!is_string($request)) return $request;
-
-            // convert special character to html entities
-            $xhtml = htmlspecialchars($request);
-
-            // strip html and php tags
+            $filter = filter_var($request, FILTER_SANITIZE_STRING);
+            $xhtml = htmlspecialchars($filter);
             return strip_tags($xhtml);
         }, $_REQUEST);
     }
@@ -40,11 +29,9 @@ class Request
      */
     public static function uri(): string
     {
-        // trimmed url
         $url = trim($_SERVER['REQUEST_URI'], '/');
-        // url w/out query string
-        $base_url = parse_url($url, PHP_URL_PATH);
-        return $base_url;
+        $filter = filter_var($url, FILTER_SANITIZE_URL);
+        return parse_url($filter, PHP_URL_PATH);
     }
 
     /**
@@ -52,31 +39,22 @@ class Request
      */
     public static function method(): string
     {
-        // forced uppercase HTTP method
-        return strtoupper($_SERVER['REQUEST_METHOD']);
+        $method = filter_var($_SERVER['REQUEST_METHOD'], FILTER_SANITIZE_STRING);
+        return strtoupper($method);
     }
 
     /**
      * Request query string
-     * 
-     * @param string $key
-     * @return mixed
      */
     public static function query(string $key = null): mixed
     {
-        // if key is null, return all get request
         if (is_null($key)) return $_GET;
 
-        // return specific get request
         return $_GET[$key] ?? null;
     }
 
     /**
      * Set Request attributes
-     * 
-     * @param string $key
-     * @param mixed $value
-     * @return void
      */
     public function __set(string $key, mixed $value): void
     {
@@ -85,22 +63,19 @@ class Request
 
     /**
      * Get Request attribute
-     * 
-     * @param string $key
-     * @return mixed
      */
     public function __get(string $key): mixed
     {
-        // check if $key exists on Request attributes
         if (!array_key_exists($key, $this->attributes)) {
-            throw new \Exception("Property {$key} doesn't exists on class " . __CLASS__);
+            throw new \Exception(
+                sprintf("Property %s doesn't exists on class %s.", $key, __CLASS__)
+            );
         }
 
         return $this->attributes[$key];
     }
 
     /**
-     * TODO: add security & refactor
      * Get a request based on request method
      */
     public static function request($key)
@@ -116,7 +91,10 @@ class Request
         return null;
     }
 
-    public function all()
+    /**
+     * Return all attributes
+     */
+    public function all(): array
     {
         return $this->attributes;
     }
