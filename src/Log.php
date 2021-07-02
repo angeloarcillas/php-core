95b783db0fd9e9fc9a9a86ba8946748cbebd9c9e
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-07-02 18:57:30 +0800

    Add comments & request client ip

--- src/Log.php
+++ src/Log.php
@@ -2,22 +2,32 @@
 
 namespace Zeretei\PHPCore;
 
-use DateTime;
-use Zeretei\PHPCore\Http\Request;
+use \DateTime;
+use \Zeretei\PHPCore\Application;
+use \Zeretei\PHPCore\Http\Request;
 
 class Log
 {
-    public static function set($error)
+    /**
+     * Create a log entry
+     */
+    public static function set(string $error): void
     {
+        $start = "=============================================================\n";
+        $end = "\n=============================================================\n";
         $date = (new DateTime())->format('Y-m-d H:i:s');
+        $ip = Request::ip();
         $route = Request::uri();
         $method = Request::method();
 
-        $start = "=============================================================\n";
-        $end = "\n=============================================================\n";
-        $message = $start . sprintf('%s - [%s] %s  %s', $date, $method, $route . PHP_EOL, $error) . $end;
+        // ============================================================
+        // 2021-12-31 - [GET] www.samplewebsite.com/users - 192.168.0.1
+        //  Table: "users" does not exists on Database: samplewebsite
+        // ============================================================
+        $message = $start . sprintf('%s - [%s] %s %s  %s', $date, $method, $route, $ip . PHP_EOL, $error) . $end;
 
         $file = Application::get('path.app') . "/logs/log.txt";
+
         file_put_contents($file, $message, FILE_APPEND);
     }
 }
