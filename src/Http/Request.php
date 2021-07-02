099c79015559481eadb8aa99c319d9acd96dc454
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-07-02 18:56:49 +0800

    Add get client ip method

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -120,4 +120,20 @@ class Request
     {
         return $this->attributes;
     }
+
+    /**
+     * Get request client IP
+     */
+    public static function ip()
+    {
+        if (isset($_SERVER['HTTP_CLIENT_IP'])) {
+            return $_SERVER['HTTP_CLIENT_IP'];
+        }
+
+        if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            return $_SERVER['HTTP_X_FORWARDED_FOR'];
+        }
+
+        return $_SERVER['REMOTE_ADDR'];
+    }
 }
