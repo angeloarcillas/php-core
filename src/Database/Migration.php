a18a32f465198b7a01e8822d78e51871789a8a2c
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-07-05 12:42:07 +0800

    Add validations & refactor

--- src/Database/Migration.php
+++ src/Database/Migration.php
@@ -2,10 +2,79 @@
 
 namespace Zeretei\PHPCore\Database;
 
-use Zeretei\PHPCore\Application;
+use \Zeretei\PHPCore\Application;
 
 class Migration
 {
+    /**
+     * Apply all available migration
+     */
+    public function apply()
+    {
+        if (!Application::exitst('database')) {
+            throw new \Exception("Please setup a database.");
+        }
+
+        if (!Application::exitst('path.databases')) {
+            throw new \Exception("Please setup a migration path.");
+        }
+
+        $dir = Application::get('path.databases') . '/migrations';
+
+        if (!file_exists($dir) && !is_dir($dir)) {
+            throw new \Exception(
+                sprintf('Directory: "%s" does not exists.', $dir)
+            );
+        }
+
+        $this->createMigrationsTable();
+
+        $appliedMigrations = $this->getAppliedMigrations();
+
+        $path = Application::get('path.databases') . '/migrations';
+        $migrations = array_diff(scandir($path), ['.', '..']);
+        $toApplyMigrations = array_diff($migrations, $appliedMigrations);
+
+        $newMigrations = [];
+
+        foreach ($toApplyMigrations as $migration) {
+            $path = Application::get('path.databases');
+            require_once $path . '/migrations/' . $migration;
+
+            $class = $this->getClassname($migration);
+
+            if (!class_exists($class)) {
+                throw new \Exception(
+                    sprintf('Class: "%s" does not exists on FILE: "%s".', $class, $migration)
+                );
+            }
+
+            $object = new $class();
+
+            if (!method_exists($object::class, 'up')) {
+                throw new \Exception(sprintf(
+                    'Method: "up()" does not exists on Class: "%s" in File: "%s".',
+                    $object::class,
+                    $migration
+                ));
+            }
+
+            $sql = $object->up();
+
+            if (!is_string($sql)) {
+                throw new \Exception("Method: up() must return a sql syntax.");
+            }
+
+            Application::get('database')->execute($sql);
+
+            $newMigrations[] = $migration;
+        }
+
+        $this->saveMigrations($newMigrations);
+
+        return $newMigrations;
+    }
+
     /**
      * Create applied migrations table
      */
@@ -21,45 +90,13 @@ class Migration
     }
 
     /**
-     * Apply all available migration
-     */
-    public function apply(): bool
-    {
-        $this->createMigrationsTable();
-
-        $appliedMigrations = $this->getAppliedMigrations();
-
-        if (!Application::exitst('path.database')) {
-            throw new \Exception("Please register a migration path to container.");
-        }
-
-        $path = Application::get('path.database') . '/migrations';
-        $migrations = array_diff(scandir($path), ['.', '..']);
-        $toApplyMigrations = array_diff($migrations, $appliedMigrations);
-
-        $newMigrations = [];
-        foreach ($toApplyMigrations as $migration) {
-            $class = $this->getClassname($migration);
-            $path = Application::get('path.database');
-            require_once $path . '/migrations/' . $migration;
-            $object = new $class();
-            $sql = $object->up();
-            Application::get('database')->execute($sql);
-            $newMigrations[] = $migration;
-        }
-
-        $this->saveMigrations($newMigrations);
-        return true;
-    }
-
-    /**
      * Get all applied migration
      */
     protected function getAppliedMigrations(): array
     {
         $sql = "SELECT migration FROM migrations";
         $migrations = Application::get('database')->fetchAll($sql);
-        return  array_map(fn ($i) => $i->migration, $migrations);
+        return  array_map(fn ($i) => $i['migration'], $migrations);
     }
 
     /**
@@ -70,10 +107,10 @@ class Migration
         $count = count($migrations);
 
         if ($count === 0) return false;
-        
+
         $params = trim(str_repeat("(?),", $count), ',');
         $sql = "INSERT INTO migrations (migration) VALUES $params";
-        
+
         return Application::get('database')->query($sql, $migrations);
     }
 
