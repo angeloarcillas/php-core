7b805bb70d93263b4a222c5426dff0723f78aebe
Angelo Arcillas <angeloarcillas64@gmail.com>
2022-03-22 10:56:01 +0800

    Improve comments

--- src/Http/Traits/Validator.php
+++ src/Http/Traits/Validator.php
@@ -25,6 +25,9 @@ trait Validator
      */
     protected $input;
 
+    // private const PASSED = true;
+    // private const FAILED = false;
+
     /**
      * Validate input fields with rules
      */
@@ -51,9 +54,10 @@ trait Validator
 
             // if "required|min:6|max:255"  (string with `|`)
             // convert to ["required", "min:6", "max:255"]
-            $rules = (!is_string($rules)) ? $rules : explode("|", $rules);
+            $rules = !is_string($rules) ? $rules : explode("|", $rules);
 
             foreach ($rules as $rule) {
+                //! change to false (tests should pass)
                 if ($this->execute($rule, $value)) break;
             }
 
@@ -67,7 +71,7 @@ trait Validator
 
 
     /**
-     * Check if request is set and has a value
+     * Check if the request is set and has a value
      */
     protected function required(string $request): bool
     {
@@ -79,7 +83,7 @@ trait Validator
     }
 
     /**
-     * Check if Request is a string
+     * Check if the Request is a string
      */
     protected function string(string $request): bool
     {
@@ -92,7 +96,7 @@ trait Validator
     }
 
     /**
-     * Check if rule minimum value meet
+     * Check if the rule minimum value passed
      */
     protected function min(string  $request, string $value)
     {
@@ -110,7 +114,7 @@ trait Validator
 
 
     /**
-     * Check if rule maximum value didn't exceed
+     * Check if the rule maximum value didn't exceed
      */
     protected function max(string $request, string $value): bool
     {
@@ -127,7 +131,7 @@ trait Validator
     }
 
     /**
-     * Check if request is a valid email
+     * Check if the request is a valid email address
      */
     protected function email($request): bool
     {
@@ -140,7 +144,7 @@ trait Validator
     }
 
     /**
-     * Check if 2 inputs matched
+     * Check if the 2 inputs matched
      */
     protected function same(string $request, string $key)
     {
@@ -153,7 +157,7 @@ trait Validator
     }
 
     /**
-     * Check if input and confirm input matched
+     * Check if the input and the confirm input matched
      */
     protected function confirm(string $request)
     {
@@ -183,7 +187,7 @@ trait Validator
         $sanitizedEmail = filter_var($email, FILTER_SANITIZE_EMAIL);
         $isFilterValidEmail = filter_var($sanitizedEmail, FILTER_VALIDATE_EMAIL);
         $isRegexValidEmail = preg_match('/^[\w\.]{3,50}@\w{2,12}\.\w{2,8}$/', $sanitizedEmail);
-        return ($isFilterValidEmail && $isRegexValidEmail);
+        return (bool) ($isFilterValidEmail && $isRegexValidEmail);
     }
 
     /**
